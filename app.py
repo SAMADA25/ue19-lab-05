918cc5355b31802ee64a384007108697a188df7e
exoevel <gabrielbergen25@gmail.com>
2024-11-19 12:57:53 +0100

    update app.py  - temp changing value for hard coding

--- app.py
+++ app.py
@@ -11,21 +11,22 @@ def get_crypto_price(crypto_id):
 if __name__ == "__main__":
     print("---Welcome to the Cryptocurrency Price Checker---")
     print("This program will give you the current price of any cryptocurrency you want.")
-    print("")
-    while True:
-        print("1. Get the price of bitcoin")
-        action = 1
-        if action == 'quit':
-            break
-        else:
-            match action:
-                case '1':
-                    actions_passed = False
-                    while actions_passed == False:
-                        try:
-                            get_crypto_price("bitcoin")
-                            actions_passed = True
-                        except KeyError:
-                            print("Invalid Cyptocurrency Name. Please try again.")
-
+    print(get_crypto_price("bitcoin"))
+#    while True:
+#        print("1. Get the price of bitcoin")
+#        action = 1
+#         if action == 'quit':
+#             break
+#         else:
+#             match action:
+#                 case '1':
+#                     actions_passed = False
+#                     while actions_passed == False:
+#                         try:
+#                             get_crypto_price("bitcoin")
+#                             actions_passed = True
+#                             break
+#                         except KeyError:
+#                             print("Invalid Cyptocurrency Name. Please try again.")
+#        break
 #a faire
