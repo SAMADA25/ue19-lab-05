5a6a7664c57c982c9044c5d786dd32588b2af135
exoevel <gabrielbergen25@gmail.com>
2024-11-19 11:06:32 +0100

    update app.py  - adding todo

--- app.py
+++ app.py
@@ -28,3 +28,5 @@ if __name__ == "__main__":
                             actions_passed = True
                         except KeyError:
                             print("Invalid Cyptocurrency Name. Please try again.")
+
+#a faire
