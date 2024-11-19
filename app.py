a42c4b915aa499d5b351426519ff25b89dffc048
exoevel <gabrielbergen25@gmail.com>
2024-11-19 10:58:09 +0100

    update app.py  - adding menu  - adding formating  - adding error handeling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_crypto_price(crypto_id):
+def get_crypto_price():
     crypto_id = input("Enter the name of the cryptocurrency: ")
     url = f"https://api.coincap.io/v2/assets/{crypto_id}"
     response = requests.get(url)
@@ -10,4 +10,21 @@ def get_crypto_price(crypto_id):
     return
 
 if __name__ == "__main__":
-    get_crypto_price('bitcoin')
+    print("---Welcome to the Cryptocurrency Price Checker---")
+    print("This program will give you the current price of any cryptocurrency you want.")
+    print("")
+    while True:
+        print("1. Get the price of a cryptocurrency")
+        action = input("What you want to do? (Enter 'quit' to exit): ")
+        if action == 'quit':
+            break
+        else:
+            match action:
+                case '1':
+                    actions_passed = False
+                    while actions_passed == False:
+                        try:
+                            get_crypto_price()
+                            actions_passed = True
+                        except KeyError:
+                            print("Invalid Cyptocurrency Name. Please try again.")
