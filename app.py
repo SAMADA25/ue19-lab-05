9fec54d331eb9f164423cba3b71800604f467de9
exoevel <gabrielbergen25@gmail.com>
2024-11-19 10:37:35 +0100

    update app.py  - formating the price output

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def get_crypto_price(crypto_id):
     url = f"https://api.coincap.io/v2/assets/{crypto_id}"
     response = requests.get(url)
     data = response.json()
-    price = data['data']['priceUsd']
-    print(f"The price of {crypto_id} is ${price}")
+    price = float(data['data']['priceUsd'])
+    print(f"The price of {crypto_id} is ${price:,.2f}")
     return
 
 if __name__ == "__main__":
